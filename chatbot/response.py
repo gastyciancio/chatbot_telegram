e52fbdb5522ea4b4c75b5c75dcae2278325b5760
gasty tesina <gastonciancio4@gmail.com>
2023-12-03 21:50:33 -0300

    agregue contexto en conversacion

--- chatbot/response.py
+++ chatbot/response.py
@@ -1,8 +1,7 @@
 from chatbot.queries import parse_response, search_id_to_QAwiki, search_item_to_QAwiki
 import pdb
 import random
-from chatbot.utils import parse_similar_question, save_answer, search_cached_answer, search_template_chatbot, send_email_to_qawiki, valid_question
-from qa_autocomplete.utils import read_json, save_json
+from chatbot.utils import parse_similar_question, save_answer, save_response_as_template_chatbot, search_cached_answer, search_question_template_en, search_template_chatbot, send_email_to_qawiki, valid_question
 
 CACHED_QUESTIONS_TEMPLATES_CHATBOT_PATH = "static/cached_questions/templates_chatbot.json"
 CACHED_QUESTIONS_TEMPLATES_PATH = "static/cached_questions/templates.json"
@@ -11,27 +10,7 @@ CACHED_PATH = 'static/cached_questions'
 def respond_to(input_text, context):
     user_message = str(input_text)
     if (user_message.lower() == 'it was helpful'):
-        if 'We had to use similar questions to give you this answer, please help us saying if this answer helped you.' in context.user_data.get('posible_response'):
-            context.user_data['posible_response'] = context.user_data.get('posible_response').replace('We had to use similar questions to give you this answer, please help us saying if this answer helped you.', ' ')
-        if "We don't have an answer for that question. However we found similar questions that might be useful. " in context.user_data.get('posible_response'):
-            context.user_data['posible_response'] = context.user_data.get('posible_response').replace("We don't have an answer for that question. However we found similar questions that might be useful. ", ' ')
-        save_answer(context.user_data.get('posible_question'), context.user_data.get('posible_response'), [], [])
-        context.user_data['posible_question'] = None
-        context.user_data['posible_response'] = None
-        template_chatbot = read_json(CACHED_QUESTIONS_TEMPLATES_CHATBOT_PATH)
-        templates = read_json(CACHED_QUESTIONS_TEMPLATES_PATH)
-        questions_template_en = []
-        for template_chat in template_chatbot:
-            questions_template_en.append(template_chat['question_template_en'].lower())
-        for temp in templates:
-            if 'question_template_en' in temp:
-                questions_template_en.append(temp['question_template_en'].lower())
-        for template in context.user_data.get('posibles_alias'):
-            if template['question_template_en'].lower() not in questions_template_en:
-                template_chatbot.append(template)
-        save_json(template_chatbot, CACHED_PATH, 'templates_chatbot.json')
-        context.user_data['posibles_alias'] = []
-       
+        save_response_as_template_chatbot(context)
         return {
             "answer" :              random.choice(["Okay, see you later!", "I'm glad it helped you", "See u till next time!"]),
             "analogous_questions":  [],
@@ -48,13 +27,6 @@ def respond_to(input_text, context):
             "general_questions":    [],
             'ask_for_add_alias':    False
         }
-    if (user_message.lower() == 'no one of them'):
-        return {
-            "answer" :              "Sorry we can't help you",
-            "analogous_questions":  [],
-            "general_questions":    [],
-            'ask_for_add_alias':    False
-        }
     elif (user_message.lower() == 'bye'):
         response = random.choice(["Bye.", "Okay, see you later!", "Goodbye.", "Hope I helped you!"])
         return {
@@ -64,23 +36,47 @@ def respond_to(input_text, context):
             'ask_for_add_alias':    False
         }
     elif not valid_question(user_message):
-        return {
-            "answer" :              'Sorry, the question must start with "What", "Which", "Where", "When", "How", "Is", "Did", "Do", "In", "Who", "On", "From", "Has", "Was" or "Are',
-            "analogous_questions":  [],
-            "general_questions":    [],
-            'ask_for_add_alias':    False
-        }
+        if context.user_data.get('context_question') == None:
+            return {
+                "answer" :              'Sorry, the question must start with "What", "Which", "Where", "When", "How", "Is", "Did", "Do", "In", "Who", "On", "From", "Has", "Was" or "Are',
+                "analogous_questions":  [],
+                "general_questions":    [],
+                'ask_for_add_alias':    False
+            }
+        else: 
+            context_question = context.user_data.get('context_question')
+            context_question_template_en = context.user_data.get('context_question_template_en')
+            response, used_similar_mentions = parse_similar_question(user_message, context, [context_question], context_question, context_question_template_en )
+            if response != "" and response != None:
+                context.user_data["posible_response"] = response
+                context.user_data["posible_question"] = user_message
+                
+                return {
+                    "answer" :              response,
+                    "analogous_questions":  [],
+                    "general_questions":    [],
+                    'ask_for_add_alias':    True
+                }
+            else:
+                send_email_to_qawiki(user_message, 'There was no answer for a question, even using similar question')
+                return {
+                    "answer" :              "There is no information we have an answer",
+                    "analogous_questions":  [],
+                    "general_questions":    [],
+                    'ask_for_add_alias':    False
+                }
     else:
         cached_response = search_cached_answer(user_message)
         if cached_response == None:
             response_QAwiki_id, similar_questions = search_id_to_QAwiki(user_message)
             if response_QAwiki_id == None:
-
                 search_template_chatbot_response = search_template_chatbot(user_message)
                 if search_template_chatbot_response['query'] != None:
                     response = parse_response(user_message, search_template_chatbot_response['query'], [],[])
                     if (response["answer"]) != 'Unexpected error. Please contact the administrator':
-                        save_answer(user_message, response["answer"], [],[])
+                        save_answer(user_message, response["answer"], [],[], search_template_chatbot_response['query_template_en'])
+                        context.user_data['context_question'] = user_message
+                        context.user_data['context_question_template_en'] = search_template_chatbot_response['query_template_en']
                     else:
                         send_email_to_qawiki(user_message, 'There was an error using the sparql given for the question on chatbot templates for: ' + user_message)
                     return {
@@ -90,10 +86,14 @@ def respond_to(input_text, context):
                         'ask_for_add_alias':    False
                     }
                 else:
-                    response = parse_similar_question(user_message, context, similar_questions)
-                    if response != "":
+                    response, used_similar_mentions = parse_similar_question(user_message, context, similar_questions, None, None)
+                    if response != "" and response != None:
                         context.user_data["posible_response"] = response
                         context.user_data["posible_question"] = user_message
+
+                        if len(used_similar_mentions) > 0:
+                            context.user_data['context_question_template_en'] = used_similar_mentions[0]
+                            context.user_data['context_question'] = user_message
                        
                         return {
                             "answer" :              response,
@@ -121,7 +121,11 @@ def respond_to(input_text, context):
                     }
                 response = parse_response(user_message, response_QAwiki_query["query"], response_QAwiki_query["analogous_questions"], response_QAwiki_query["general_questions"])
                 if (response["answer"]) != 'Unexpected error. Please contact the administrator':
-                    save_answer(user_message, response["answer"], response["analogous_questions"], response["general_questions"])
+                    response_search_question_template_en = search_question_template_en(user_message)
+                    save_answer(user_message, response["answer"], response["analogous_questions"], response["general_questions"], response_search_question_template_en)
+                    
+                    context.user_data['context_question'] = user_message
+                    context.user_data['context_question_template_en'] = response_search_question_template_en
                 else:
                     send_email_to_qawiki(user_message, 'There was an error using the sparql given for the question on QAWiki with id' + response_QAwiki_id)
                 return {
@@ -131,6 +135,8 @@ def respond_to(input_text, context):
                     'ask_for_add_alias':    False
                 }
         else:
+            context.user_data['context_question'] = user_message
+            context.user_data['context_question_template_en'] = cached_response['question_template_en']
             return {
                     "answer" :              cached_response['answer'],
                     "analogous_questions":  cached_response['analogous_questions'],
--- chatbot/utils.py
+++ chatbot/utils.py
@@ -5,7 +5,7 @@ from fuzzywuzzy import fuzz, process
 import requests
 from datetime import datetime
 import sentry_sdk
-from chatbot.openIA import search_entity_in_chatgpt
+import spacy
 import pdb
 from qa_autocomplete.utils import read_json, save_json
 from sentence_transformers import SentenceTransformer, util
@@ -154,63 +154,78 @@ def search_label(id, endpoint):
     except:
         return None
 
-def parse_similar_question(original_question, context,similar_questions):
+def parse_similar_question(original_question, context,similar_questions, context_question = None, context_question_template_en = None):
+    response_entity_original_question, error = search_entity_using_main_entity_searcher(original_question)
 
-   response_entity_original_question_in_chatgtp, error = search_entity_in_chatgpt(original_question)
-   responses_ids_entities_original_question = search_for_entities_in_wikidata(response_entity_original_question_in_chatgtp)
+    if error != None:
+       return None, []
 
-   if error != None:
-        return None
-   else:
-        response_search_instance_of_entities_original_question = search_instance_of(responses_ids_entities_original_question)
-        templates = read_json(CACHED_QUESTIONS_TEMPLATES_PATH)
-        templates_chatbot = read_json(CACHED_QUESTIONS_TEMPLATES_CHATBOT_PATH)
-        ids_of_matches_template = []
-        templates_question_template_en = []
-    
-        for similar_question in similar_questions:
-            for template in templates:
-                if 'question_en' in template and template['question_en'].lower() == similar_question.lower():
+    responses_ids_entities_original_question = search_for_entities_in_wikidata(response_entity_original_question)
+    response_search_instance_of_entities_original_question = search_instance_of(responses_ids_entities_original_question)
+    templates = read_json(CACHED_QUESTIONS_TEMPLATES_PATH)
+    templates_chatbot = read_json(CACHED_QUESTIONS_TEMPLATES_CHATBOT_PATH)
+    ids_of_matches_template = []
+    templates_question_template_en = []
+
+    for similar_question in similar_questions:
+        for template in templates:
+            if 'question_en' in template and template['question_en'].lower() == similar_question.lower():
+                for match_en in template["matches_en"]:
+                    ids_of_matches_template.append(match_en['entity'])
+            if 'question_template_en' in template:
+                templates_question_template_en.append(template['question_template_en'])
+                if context_question != None and context_question_template_en.lower() == template['question_template_en'].lower():
                     for match_en in template["matches_en"]:
                         ids_of_matches_template.append(match_en['entity'])
-                if 'question_template_en' in template:
-                    templates_question_template_en.append(template['question_template_en'])
-            for template in templates_chatbot:
-                if 'question_en' in template and template['question_en'].lower() == similar_question.lower():
+
+        for template in templates_chatbot:
+            if 'question_en' in template and template['question_en'].lower() == similar_question.lower():
+                for match_en in template["matches_en"]:
+                    ids_of_matches_template.append(match_en['entity'])
+            if 'question_template_en' in template:
+                templates_question_template_en.append(template['question_template_en'])
+                if context_question != None and context_question_template_en.lower() == template['question_template_en'].lower():
                     for match_en in template["matches_en"]:
                         ids_of_matches_template.append(match_en['entity'])
-                if 'question_template_en' in template:
-                    templates_question_template_en.append(template['question_template_en'])
-        
-        set_transform_templates_question_template_en = set(templates_question_template_en)
-        set_transform_ids_of_matches_template= set(ids_of_matches_template)
 
-        templates_question_template_en = list(set_transform_templates_question_template_en)
-        ids_of_matches_template = list(set_transform_ids_of_matches_template)
+    set_transform_templates_question_template_en = set(templates_question_template_en)
+    set_transform_ids_of_matches_template= set(ids_of_matches_template)
 
-        response_search_instance_of_entities_template = search_instance_of(ids_of_matches_template)
+    templates_question_template_en = list(set_transform_templates_question_template_en)
+    ids_of_matches_template = list(set_transform_ids_of_matches_template)
+    response_search_instance_of_entities_template = search_instance_of(ids_of_matches_template)
 
-        mentions_template = []
-        posibles_ids_for_the_original_question = set()
-        for entities_and_mention_template in response_search_instance_of_entities_template:
-            mentions_template.append(entities_and_mention_template['id_mention'])
-       
-        entity = ''
-        for entity_and_mention_original_question in response_search_instance_of_entities_original_question:
-            if entity_and_mention_original_question['id_mention'] in mentions_template:
-                posibles_ids_for_the_original_question.add(entity_and_mention_original_question['id_entity'])
-                entity = entity_and_mention_original_question['label']
-        final_response = ""
-        lista_ids = list(posibles_ids_for_the_original_question)
-        for posibles_id in lista_ids:
-            response = similar_query(posibles_id, similar_questions, entity, original_question)
-            if response['final_answer'] != "":
-                final_response = final_response + response["final_answer"] + "\n"
-                mentions = find_by_key(response_search_instance_of_entities_original_question, 'id_entity', posibles_id)
-                sparql_set = set(response['sparql_values'])
-                array_sparql = list(sparql_set)
+    mentions_template = []
+    posibles_ids_for_the_original_question = set()
+    for entities_and_mention_template in response_search_instance_of_entities_template:
+        mentions_template.append(entities_and_mention_template['id_mention'])
+
+    entity = ''
+    for entity_and_mention_original_question in response_search_instance_of_entities_original_question:
+        if entity_and_mention_original_question['id_mention'] in mentions_template:
+            posibles_ids_for_the_original_question.add(entity_and_mention_original_question['id_entity'])
+            entity = entity_and_mention_original_question['label']
+    final_response = ""
+    lista_ids = list(posibles_ids_for_the_original_question)
+    used_similar_mentions = set()
+    for posibles_id in lista_ids:
+        response = similar_query(posibles_id, similar_questions, entity, original_question, context_question_template_en)
+        if response['final_answer'] != "":
+            final_response = final_response + response["final_answer"] + "\n"
+            mentions = find_by_key(response_search_instance_of_entities_original_question, 'id_entity', posibles_id)
+            sparql_set = set(response['sparql_values'])
+            for used_similar_question in response['similar_questions_used']:
+                for template in templates:
+                    if 'question_en' in template and used_similar_question.lower() == template['question_en'].lower():
+                        used_similar_mentions.add(template['question_template_en'])
+                for template in templates_chatbot:
+                    if 'question_en' in template and used_similar_question.lower() == template['question_en'].lower():
+                        used_similar_mentions.add(template['question_template_en'])
+
+            array_sparql = list(sparql_set)
+            if valid_question(original_question):
                 add_template_chatbot_to_context(mentions[0]['label'], posibles_id, original_question, array_sparql, templates_question_template_en,context)
-        return final_response
+    return final_response, list(used_similar_mentions)
 
 def find_by_key(array, clave, valor):
     return [elemento for elemento in array if elemento.get(clave) == valor]
@@ -425,7 +440,7 @@ def valid_question(text):
             break
     return start_with
 
-def save_answer(question, answer, analogous_questions, general_questions):
+def save_answer(question, answer, analogous_questions, general_questions, question_template_en= None):
     answers = read_json(CACHED_ANSWERS_TEMPLATES_PATH)
     already_exists = False
     for answer_cached in answers:
@@ -436,6 +451,7 @@ def save_answer(question, answer, analogous_questions, general_questions):
             {
                 "question": question,
                 "answer":   answer,
+                "question_template_en" : question_template_en,
                 "analogous_questions": analogous_questions,
                 "general_questions": general_questions
             }
@@ -454,7 +470,7 @@ def answers_reset():
     answers = []
     save_json(answers, CACHED_PATH, ANSWERS_FILENAME)
 
-def similar_query(id_entity_selected, similar_questions, entity, original_question):
+def similar_query(id_entity_selected, similar_questions, entity, original_question, context_question_template_en):
     templates = read_json(CACHED_QUESTIONS_TEMPLATES_PATH)
     templates_chatbot = read_json(CACHED_QUESTIONS_TEMPLATES_CHATBOT_PATH)
     initial_response = "We don't have an answer for that question. However we found similar questions that might be useful. \n"
@@ -463,14 +479,16 @@ def similar_query(id_entity_selected, similar_questions, entity, original_questi
     sparql_of_similar_questions = []
     sparql_values = []
     change_response = False
+    count_results = 0
     for similar_question in similar_questions:
         for template in templates:
-            if 'question_en' in template and template['question_en'].lower() == similar_question.lower() and template['visible_question_en'].count('{') <= 1:
+            if (('question_en' in template and template['question_en'].lower() == similar_question.lower()) or ('question_en' in template and context_question_template_en != None and template['question_template_en'].lower() == context_question_template_en.lower())) and template['visible_question_en'].count('{') <= 1 and template['query_template_en'].count('entity_') <= 1:
                 sparql_of_similar_question = template['query_template_en']
                 if sparql_of_similar_question not in sparql_of_similar_questions:
                     sparql_of_similar_questions.append(sparql_of_similar_question)
                     response = search_with_sparql_of_similar_question(sparql_of_similar_question, id_entity_selected)
                     if response['answer'] != "":
+                        count_results = count_results + 1
                         similar_questions_used.add(similar_question)
                         sparql_values.append(response['sparql_value'])
                         if template['question_en'].replace(template['matches_en'][0]['mention'], entity).lower() == original_question:
@@ -479,12 +497,13 @@ def similar_query(id_entity_selected, similar_questions, entity, original_questi
                         else:
                             final_response = final_response + 'Having the question "'+ template['question_en'].replace(template['matches_en'][0]['mention'], entity) +'" the answer is: '+ response['answer'] + "\n"
         for template in templates_chatbot:
-            if 'question_en' in template and template['question_en'].lower() == similar_question.lower():
+            if ('question_en' in template and template['question_en'].lower() == similar_question.lower()) or ('question_en' in template and context_question_template_en != None and template['question_template_en'].lower() == context_question_template_en.lower()):
                 sparql_of_similar_question = template['query_template_en']
                 if sparql_of_similar_question not in sparql_of_similar_questions:
                     sparql_of_similar_questions.append(sparql_of_similar_question)
                     response = search_with_sparql_of_similar_question(sparql_of_similar_question, id_entity_selected)
                     if response['answer'] != "":
+                        count_results = count_results + 1
                         similar_questions_used.add(similar_question)
                         sparql_values.append(response['sparql_value'])
                         if template['question_en'].replace(template['matches_en'][0]['mention'], entity).lower() == original_question:
@@ -492,10 +511,14 @@ def similar_query(id_entity_selected, similar_questions, entity, original_questi
                             final_response = final_response + 'The answer to "'+ template['question_en'].replace(template['matches_en'][0]['mention'], entity) +'" is: '+ response['answer'] + "\n"
                         else:
                             final_response = final_response + 'Having the question "'+ template['question_en'].replace(template['matches_en'][0]['mention'], entity) +'" the answer is: '+ response['answer'] + "\n"
+    
     if change_response == True:
         initial_response = ''
+    final_answer = initial_response + final_response + 'We had to use similar questions to give you this answer, please help us saying if this answer helped you.'
+    if count_results == 0:
+        final_answer = ''
     return {
-        "final_answer": initial_response + final_response + 'We had to use similar questions to give you this answer, please help us saying if this answer helped you.',
+        "final_answer": final_answer,
         "similar_questions_used": similar_questions_used,
         "sparql_values": sparql_values 
     }
@@ -541,5 +564,73 @@ def search_template_chatbot(user_message):
             elif '$entity_3' in template['query_template_en']:
                 sparql_value = template['query_template_en'].replace('$entity_3', 'wd:'+template['matches_en'][0]['entity'].upper())
             
-            return { "query": sparql_value }
-    return { "query": None }
+            return { "query": sparql_value, "query_template_en": template['query_template_en'] }
+    return { "query": None, "query_template_en": None }
+
+def save_response_as_template_chatbot(context):
+    if 'We had to use similar questions to give you this answer, please help us saying if this answer helped you.' in context.user_data.get('posible_response'):
+        context.user_data['posible_response'] = context.user_data.get('posible_response').replace('We had to use similar questions to give you this answer, please help us saying if this answer helped you.', ' ')
+    if "We don't have an answer for that question. However we found similar questions that might be useful. " in context.user_data.get('posible_response'):
+        context.user_data['posible_response'] = context.user_data.get('posible_response').replace("We don't have an answer for that question. However we found similar questions that might be useful. ", ' ')
+
+    question = context.user_data.get('posible_question')
+    answer = context.user_data.get('posible_response')
+    if not valid_question(context.user_data.get('posible_question')):
+        question = re.search(r'"([^"]*)"', answer).group(1)
+    save_answer(question, answer, [], [], context.user_data.get("context_question_template_en") )
+    context.user_data['posible_question'] = None
+    context.user_data['posible_response'] = None
+    template_chatbot = read_json(CACHED_QUESTIONS_TEMPLATES_CHATBOT_PATH)
+    templates = read_json(CACHED_QUESTIONS_TEMPLATES_PATH)
+    questions_template_en = []
+    for template_chat in template_chatbot:
+        questions_template_en.append(template_chat['question_template_en'].lower())
+    for temp in templates:
+        if 'question_template_en' in temp:
+            questions_template_en.append(temp['question_template_en'].lower())
+    if (context.user_data.get('posibles_alias') != None):
+        for template in context.user_data.get('posibles_alias'):
+            if template['question_template_en'].lower() not in questions_template_en:
+                template_chatbot.append(template)
+    save_json(template_chatbot, CACHED_PATH, 'templates_chatbot.json')
+    context.user_data['posibles_alias'] = []
+
+def search_question_template_en(question):
+    template_chatbot = read_json(CACHED_QUESTIONS_TEMPLATES_CHATBOT_PATH)
+    templates = read_json(CACHED_QUESTIONS_TEMPLATES_PATH)
+
+    for template in templates:
+        if 'question_en' in template and template['question_en'].lower() == question.lower():
+            return template['question_template_en']
+    for template in template_chatbot:
+        if 'question_en' in template and template['question_en'].lower() == question.lower():
+            return template['question_template_en']
+    
+    return ''
+
+def search_entity_using_main_entity_searcher(question):
+    try:
+       
+        response = identify_main_entity(question)
+        
+        if response != '':
+            return response, None
+        else:
+            return None, 'No se encontraron respuestas con main identifier'
+
+    except Exception as e:
+        print(f"Respuesta de Main Entity searcher: {e}")
+        return None, str(e)
+
+def identify_main_entity(question):
+
+    nlp = spacy.load("en_core_web_sm")
+    text = question
+    documen = nlp(text)
+
+    if len(documen.ents) > 0:
+        print(f'Main Entity found: {documen.ents[0].text}')
+        return documen.ents[0].text
+    else:
+        print(f'Main Entity Not found')
+        return ''
