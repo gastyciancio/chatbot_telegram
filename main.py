c454e0a70ebf103862ac8291e42408527898ac15
gaston <gaston@pruebatesis.myguest.virtualbox.org>
2024-02-10 15:58:13 -0300

    docker compose working with persistance

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ def main():
     create_answers_file()
     sched = BackgroundScheduler()
     sched.add_job(templates_update, 'interval', args=[QAWIKI_ENDPOINT, QAWIKI_ENTITY_PREFIX, logger], minutes=JOB_INTERVAL_MINUTES, next_run_time=datetime.datetime.now())
-    sched.add_job(answers_reset, 'interval', minutes=JOB_INTERVAL_MINUTES, next_run_time=datetime.datetime.now())
+    sched.add_job(answers_reset, 'interval', minutes=JOB_INTERVAL_MINUTES, next_run_time=datetime.datetime.now() + datetime.timedelta(minutes=5) )
     sched.start()
 
     try:
